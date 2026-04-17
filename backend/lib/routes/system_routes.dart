a60bf0aaf25e49647941847adfefd076df361dc8
unknown <hybiekay2@gmail.com>
2026-04-17 23:00:33 +0100

    chore: auto-save frontend

---
+++ backend/lib/routes/system_routes.dart
@@ -0,0 +1,16 @@
+import 'package:backend/controllers/system_controller.dart';
+import 'package:flint_dart/flint_dart.dart';
+
+class SystemRoutes extends RouteGroup {
+  @override
+  String get prefix => '/admin/system';
+
+  @override
+  List<Middleware> get middlewares => [AuthMiddleware()];
+
+  @override
+  void register(Flint app) {
+    app.post('/update',        useController(SystemController.new, (c) => c.startUpdate()));
+    app.get('/update-status',  useController(SystemController.new, (c) => c.updateStatus()));
+  }
+}
